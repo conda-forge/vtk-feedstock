173658270695ea713d276bae1fa7faabdc439d98
Korijn van Golen <korijn@gmail.com>
2016-06-25 22:30:45 +0200

    Rerender

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -51,9 +51,15 @@ conda info
 yum install -y libXt-devel mesa-libGLU-devel
 
 
-# Embarking on 2 case(s).
+# Embarking on 3 case(s).
     set -x
     export CONDA_PY=27
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
