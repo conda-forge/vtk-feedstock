fbacab2827da8c3c3e1bb5988ca280abf586eca1
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-07-04 14:11:24 +0200

    Configured a bunch of system libs, let's see how far this gets us

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# grab a list of the packages in the build environment to determine if we are
+# building against osmesa or not
+CONDA_LST=`conda list`
+if [ `uname` = "Darwin" ] ; then
+    SCREEN_ARGS="-DVTK_USE_X:BOOL=OFF \
+        -DVTK_USE_COCOA:BOOL=ON \
+        -DVTK_USE_CARBON:BOOL=OFF"
+elif [[ ${CONDA_LST}'y' == *'osmesa'* ]]; then
+    SCREEN_ARGS="-DVTK_USE_X:BOOL=OFF \
+        -DVTK_OPENGL_HAS_OSMESA:BOOL=ON \
+        -DOPENGL_INCLUDE_DIR:PATH=${PREFIX}/include \
+        -DOPENGL_gl_LIBRARY:FILEPATH=${PREFIX}/lib/libOSMesa.so \
+        -DOPENGL_glu_LIBRARY:FILEPATH=${PREFIX}/lib/libGLU.so \
+        -DOSMESA_INCLUDE_DIR:PATH=${PREFIX}/include \
+        -DOSMESA_LIBRARY:FILEPATH=${PREFIX}/lib/libOSMesa.so"
+else
+    SCREEN_ARGS="-DVTK_USE_X:BOOL=ON"
+fi
+
 # FIXME: This is a hack to make sure the environment is activated.
 # The reason this is required is due to the conda-build issue
 # mentioned below.
@@ -13,9 +32,8 @@ cd build
 
 BUILD_CONFIG=Release
 
-# sometimes python is suffixed, these are quick fixes
+# sometimes python is suffixed, these are quick fixes to find some variables
 # in a future PR we should probably switch to cmake find python scripting
-
 PYTHON_INCLUDE="${PREFIX}/include/python${PY_VER}"
 if [ ! -d $PYTHON_INCLUDE ]; then
     PYTHON_INCLUDE="${PREFIX}/include/python${PY_VER}m"
@@ -31,8 +49,7 @@ if [ ! -f $PYTHON_LIBRARY ]; then
     PYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_VER}m.${PYTHON_LIBRARY_EXT}"
 fi
 
-# end of quick fixes
-
+# now we can start configuring
 cmake .. -G "Ninja" \
     -Wno-dev \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
@@ -46,9 +63,20 @@ cmake .. -G "Ninja" \
     -DVTK_WRAP_PYTHON:BOOL=ON \
     -DVTK_PYTHON_VERSION:STRING="${PY_VER}" \
     -DVTK_INSTALL_PYTHON_MODULE_DIR:PATH="${SP_DIR}" \
+    -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF \
     -DPYTHON_EXECUTABLE:FILEPATH=$PYTHON \
     -DPYTHON_INCLUDE_DIR:PATH=$PYTHON_INCLUDE \
     -DPYTHON_LIBRARY:FILEPATH=$PYTHON_LIBRARY \
-    -DVTK_USE_SYSTEM_LIBRARIES:BOOL=ON
+    -DVTK_USE_SYSTEM_ZLIB:BOOL=ON \
+    -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON \
+    -DVTK_USE_SYSTEM_HDF5:BOOL=ON \
+    -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON \
+    -DVTK_USE_SYSTEM_NETCDF:BOOL=ON \
+    -DVTK_USE_SYSTEM_LIBPNG:BOOL=ON \
+    -DVTK_USE_SYSTEM_JPEG:BOOL=ON \
+    -DVTK_USE_SYSTEM_TIFF:BOOL=ON \
+    -DVTK_USE_SYSTEM_BOOST:BOOL=ON \
+    ${SCREEN_ARGS}
 
+# compile & install!
 ninja install
