2d443d05c1a55779901a8229297003336434721e
Gregory R. Lee <grlee77@gmail.com>
2017-05-25 22:42:49 -0400

    MNT: Re-rendered with conda-smithy 2.3.1

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -65,15 +65,9 @@ source run_conda_forge_build_setup
 /usr/bin/sudo -n yum install -y libXt-devel mesa-libGLU-devel
 
 
-# Embarking on 2 case(s).
+# Embarking on 1 case(s).
     set -x
-    export CONDA_PY=35
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
-    export CONDA_PY=36
+    export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
