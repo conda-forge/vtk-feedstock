7b907c1b6e52067ebceecb2999a12e38658bce68
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-03-27 21:30:40 +0100

    Point cmake to png include

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,8 @@ BUILD_CONFIG=Release
 # now we can start configuring
 cmake .. -G "Ninja" \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
-    -DCMAKE_PREFIX_PATH=$PREFIX
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DPNG_PNG_INCLUDE_DIR=${PREFIX}/include
 
 # compile & install!
 ninja install -v
