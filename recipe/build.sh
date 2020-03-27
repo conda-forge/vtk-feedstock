d8c88e772c6410b04e20dd506074afa3d810e04e
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-03-27 20:49:27 +0100

    Add CMAKE_PREFIX_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ BUILD_CONFIG=Release
 
 # now we can start configuring
 cmake .. -G "Ninja" \
-    -DCMAKE_BUILD_TYPE=$BUILD_CONFIG
+    -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
+    -DCMAKE_PREFIX_PATH=$PREFIX
 
 # compile & install!
 ninja install -v
