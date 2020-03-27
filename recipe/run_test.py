edc863f64383312720ff501951d8bc0ac6a592f1
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-03-27 22:03:43 +0100

    Add include dir for libpng
    This seems to help CMake find the right version of libpng.
    
    Add a test to make sure libpng is doing the right thing.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,3 +1,31 @@
 import pkg_resources
+import vtk
+
 # If this fails it raises a DistributionNotFound exception
 pkg_resources.get_distribution('vtk')
+
+# test libpng, since this was causing trouble in OSX previously
+source = vtk.vtkCubeSource()
+
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(source.GetOutputPort())
+
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+renderer = vtk.vtkRenderer()
+renderer.AddActor(actor)
+
+window = vtk.vtkRenderWindow()
+window.AddRenderer(renderer)
+window.SetSize(500, 500)
+window.Render()
+
+window_filter = vtk.vtkWindowToImageFilter()
+window_filter.SetInput(window)
+window_filter.Update()
+
+writer = vtk.vtkPNGWriter()
+writer.SetFileName('cube.png')
+writer.SetInputData(window_filter.GetOutput())
+writer.Write()
