45bf068ce674d2ae7eb2350176a5c4eb31a4a7ba
Antoine DECHAUME <antoine.dechaume@c-s.fr>
2020-05-20 09:04:11 +0200

    Remove duplicated cmake cli argument

--- recipe/build.sh
+++ recipe/build.sh
@@ -48,7 +48,6 @@ cmake -LAH .. -G "Ninja" \
     -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF \
     -DVTK_RENDERING_BACKEND=OpenGL2 \
     -DModule_vtkRenderingMatplotlib=ON \
-    -DModule_vtkIOXdmf2:INTERNAL=ON \
     -DVTK_USE_SYSTEM_ZLIB:BOOL=ON \
     -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON \
     -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON \
