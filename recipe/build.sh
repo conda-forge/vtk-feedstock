092e972dc872d004ffd82d216b186a1d4b605f1a
Isuru Fernando <isuruf@gmail.com>
2021-03-19 14:57:05 -0500

    use CMAKE_ARGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,11 +19,11 @@ if [ -f "$PREFIX/lib/libOSMesa32${SHLIB_EXT}" ]; then
         "-DOSMESA_LIBRARY:FILEPATH=${PREFIX}/lib/libOSMesa32${SHLIB_EXT}"
     )
 
-    if [ "${OSNAME}" == Linux ]; then
+    if [[ "${travis_platform}" == linux-* ]]; then
         VTK_ARGS+=(
             "-DVTK_USE_X:BOOL=OFF"
         )
-    elif [ "${OSNAME}" == Darwin ]; then
+    elif [[ "${travis_platform}" == osx-* ]]; then
         VTK_ARGS+=(
             "-DVTK_USE_COCOA:BOOL=OFF"
             "-DCMAKE_OSX_SYSROOT:PATH=${CONDA_BUILD_SYSROOT}"
@@ -41,11 +41,11 @@ else
         "-DTCL_LIBRARY:FILEPATH=${PREFIX}/lib/libtcl${TCLTK_VERSION}${SHLIB_EXT}"
         "-DTK_LIBRARY:FILEPATH=${PREFIX}/lib/libtk${TCLTK_VERSION}${SHLIB_EXT}"
     )
-    if [ "${OSNAME}" == Linux ]; then
+    if [[ "${travis_platform}" == linux-* ]]; then
         VTK_ARGS+=(
             "-DVTK_USE_X:BOOL=ON"
         )
-    elif [ "${OSNAME}" == Darwin ]; then
+    elif [[ "${travis_platform}" == osx-* ]]; then
         VTK_ARGS+=(
             "-DVTK_USE_COCOA:BOOL=ON"
             "-DCMAKE_OSX_SYSROOT:PATH=${CONDA_BUILD_SYSROOT}"
@@ -56,7 +56,7 @@ fi
 echo "VTK_ARGS:" "${VTK_ARGS[@]}"
 
 # now we can start configuring
-cmake -LAH .. -G "Ninja" \
+cmake -LAH .. -G "Ninja" ${CMAKE_ARGS} \
     -Wno-dev \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
     -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
