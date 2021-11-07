b6a12d2a2240bd3f2a90335d3e2d1ad67739cedb
Matthias Diener <mdiener@illinois.edu>
2021-11-06 22:10:21 -0500

    disable cgns

--- recipe/build.sh
+++ recipe/build.sh
@@ -126,6 +126,7 @@ cmake -LAH .. -G "Ninja" ${CMAKE_ARGS} \
     -DVTK_MODULE_USE_EXTERNAL_VTK_pegtl:BOOL=OFF \
     -DVTK_MODULE_USE_EXTERNAL_VTK_exprtk:BOOL=OFF \
     -DVTK_MODULE_USE_EXTERNAL_VTK_fmt:BOOL=OFF \
+    -DVTK_MODULE_USE_EXTERNAL_VTK_cgns:BOOL=OFF \
     "${VTK_ARGS[@]}"
 
 # compile & install!
