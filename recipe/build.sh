6ee8b197145881dccd9173734d2f8e1b182f0da4
Tobias Fischer <info@tobiasfischer.info>
2021-11-08 07:38:09 +1000

    Try again ..

--- recipe/build.sh
+++ recipe/build.sh
@@ -127,7 +127,7 @@ cmake -LAH .. -G "Ninja" ${CMAKE_ARGS} \
     -DVTK_MODULE_USE_EXTERNAL_VTK_exprtk:BOOL=OFF \
     -DVTK_MODULE_USE_EXTERNAL_VTK_fmt:BOOL=OFF \
     -DVTK_MODULE_USE_EXTERNAL_VTK_cgns:BOOL=OFF \
-    -DVTK_MODULE_USE_EXTERNAL_VTK_seacasioss:BOOL=OFF \
+    -DVTK_MODULE_USE_EXTERNAL_VTK_ioss:BOOL=OFF \
     "${VTK_ARGS[@]}"
 
 # compile & install!
