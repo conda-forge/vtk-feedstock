4c8dfa1c8bf575044c69eb0aec0c8d7c94cd2cbe
Bas Nijholt <bas@nijho.lt>
2023-02-24 11:02:21 -0800

    Do not build Windows with FFMPEG (currently also not supported)

---
+++ recipe/build-io-ffmpeg.sh
@@ -0,0 +1,7 @@
+# Copy:
+# $PREFIX/include/vtk-9.2/vtkIOFFMPEGModule.h
+# $PREFIX/lib/libvtkIOFFMPEG-*.so.*
+# $PREFIX/lib/python3.11/site-packages/vtkmodules/vtkIOFFMPEG
+# $PREFIX/lib/python3.11/site-packages/vtkmodules/vtkIOFFMPEG.*.so
+# $PREFIX/lib/python3.11/site-packages/vtkmodules/vtkIOFFMPEG.pyi
+# $PREFIX/lib/vtk-9.2/hierarchy/VTK/vtkIOFFMPEG-hierarchy.txt
