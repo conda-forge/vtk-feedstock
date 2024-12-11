6bd3074da26cd7c25d2c1ef48bf810e48f2d9a30
Tobias Fischer <info@tobiasfischer.info>
2024-12-11 15:54:37 +1000

    fix liblzma -> lzma

--- recipe/bld-base.bat
+++ recipe/bld-base.bat
@@ -45,7 +45,7 @@ cmake .. -G "Ninja" ^
     -DVTK_MODULE_USE_EXTERNAL_VTK_cgns:BOOL=OFF ^
     -DVTK_MODULE_USE_EXTERNAL_VTK_ioss:BOOL=OFF ^
     -DVTK_MODULE_USE_EXTERNAL_VTK_verdict:BOOL=OFF ^
-    -DLZMA_LIBRARY="%LIBRARY_PREFIX%/lib/liblzma.lib" ^
+    -DLZMA_LIBRARY="%LIBRARY_PREFIX%/lib/lzma.lib" ^
     !VTK_ARGS!
 if errorlevel 1 exit 1
 
